b3f920cf564a0233fac90de3d8e5f917b53b98ad
timeil0611 <79957978+timeil0611@users.noreply.github.com>
2024-12-10 21:44:19 +0800

    Add files via upload

--- TQQQ策略(九月賣出+停損+周線上揚買入).py
+++ TQQQ策略(九月賣出+停損+周線上揚買入).py
@@ -8,161 +8,216 @@ import pandas as pd
 import talib
 from talib import abstract
 import matplotlib.pyplot as plt
+import plotly.express as px
 
 import yfinance as yf
 
+from datetime import datetime
 
-def SMA(values, n):
-    """
-    Return simple moving average of `values`, at
-    each step taking into account `n` previous values.
-    """
-    return pd.Series(values).rolling(n).mean()
+
 
 
 
 start = "1980-01-01"
 
 # 取得資料
-qqq = pd.read_csv("qqq.csv")
-tqqq_mock = pd.read_csv("TQQQ_Mock_kline.csv")
+qqq = pd.read_csv("qqq_19830810.csv")
+tqqq_mock = pd.read_csv("TQQQ_Mock_kline _19830810.csv")
 df=tqqq_mock
 
 # 設定Index
 df.set_index("Date", inplace=True)
 df.set_index(pd.DatetimeIndex(df.index), inplace=True)
 
-
-
-# backtesting.py
-# ta-lib 格式
-df2 = df.rename(
-    columns={
-        "High": "high",
-        "Low": "low",
-    }
-)
-
-# #合併資料
+#合併資料
 qqq.set_index("Date", inplace=True)
 qqq.set_index(pd.DatetimeIndex(qqq.index), inplace=True)
 qqq = qqq[start:]
 df = pd.concat([df, qqq], axis=1)
-qqq = qqq[["qqq"]]
+
+i=1
+k=1
+
+# 為了畫圖
+years = list(range(1990, 2024)) # range中。第一個要與end_date年份相同，第二個要與final_date年份+1相同
+str_result = []
+b_h_result= []
+
+# 不同windows
+start_date = "1983-08-10"
+end_date = "1990-08-10"
+final_date = "2023-10-20"
+
+# 将 end_date 转换为 datetime 对象
+end_date_dt = datetime.strptime(end_date, "%Y-%m-%d")
+
+# 提取年份并转为数字
+year = end_date_dt.year
+
+while pd.to_datetime(end_date) <= pd.to_datetime(final_date):
+    # 篩選時間範圍
+    window_df = df[(df.index >= start_date) & (df.index <= end_date)]
+    
+    # backtesting.py
+    # ta-lib 格式
+    df2 = window_df.rename(
+        columns={
+            "High": "high",
+            "Low": "low",
+        }
+    )
+
+    
+
+    def QQQ(data):  # Data is going to be our OHLCV
+        # 取得qqq值
+        return data
+
+    def SMA(values, n):
+        """
+        Return simple moving average of `values`, at
+        each step taking into account `n` previous values.
+        """
+        return pd.Series(values).rolling(n).mean()
+    
+    def SMA60(data):  # Data is going to be our OHLCV
+        # 取得SMA值
+        df2["SMA60"] = talib.SMA(df2["Close"], timeperiod=60)
+        return df2["SMA60"]
 
 
-def QQQ(data):  # Data is going to be our OHLCV
-    # 取得qqq值
-    return data
+    def EMA30(data):  # Data is going to be our OHLCV
+        # 取得EMA值
+        df2["EMA30"] = talib.EMA(df2["Close"], timeperiod=60)
+        return df2["EMA30"]
 
 
-def SMA60(data):  # Data is going to be our OHLCV
-    # 取得SMA值
-    df2["SMA60"] = talib.SMA(df2["Close"], timeperiod=60)
-    return df2["SMA60"]
+    def WMA30(data):  # Data is going to be our OHLCV
+        # 取得WMA值
+        df2["WMA30"] = talib.WMA(df2["Close"], timeperiod=30)
+        return df2["WMA30"]
 
 
-def EMA30(data):  # Data is going to be our OHLCV
-    # 取得EMA值
-    df2["EMA30"] = talib.EMA(df2["Close"], timeperiod=60)
-    return df2["EMA30"]
+    def VSMA60(data):  # Data is going to be our OHLCV
+        # 取得SMA值
+        df2["SMA60"] = talib.SMA(df2["Volume"], timeperiod=60)
+        return df2["SMA60"]
 
 
-def WMA30(data):  # Data is going to be our OHLCV
-    # 取得WMA值
-    df2["WMA30"] = talib.WMA(df2["Close"], timeperiod=30)
-    return df2["WMA30"]
+    # MA 策略
+    class TQQQStra(Strategy):
+        n1 = 10
+        cooldown_days = 4
 
-
-def VSMA60(data):  # Data is going to be our OHLCV
-    # 取得SMA值
-    df2["SMA60"] = talib.SMA(df2["Volume"], timeperiod=60)
-    return df2["SMA60"]
-
-
-# MA 策略
-class MAStra(Strategy):
-    n1 = 10
-    cooldown_days = 4
-
-    def init(self):
-        self.qqq_sma1 = self.I(SMA, self.data.qqq, self.n1)
-        self.sma1 = self.I(SMA, self.data.Close, self.n1)
-        # self.Vsma1 = self.I(SMA, self.data.Volume, self.n1)
-        # self.ema1 = self.I(EMA30, self.data)
-        self.qqq = self.I(QQQ, qqq["qqq"])
-        print(self.data)
-        self.cooldown_weeks = 6
-        self.last_trade_date = pd.to_datetime("1981-07-31")  # 設定初值
-        # 周範圍
-        self.w_qqq_sma = resample_apply(
-                    'W', SMA, self.data.qqq, 6, plot=True)
-
-    def next(self):
-        # 當前qqq價格
-        qqq_current_price = self.data["qqq"][-1]
-         # 定義一個用於存儲最高價的 Series
-        self.qqq_high_prices = self.data["qqq"]
-        # 獲取過去 80 天的最高價
-        qqq_highest_high = self.qqq_high_prices[-80:].max()
-        
-        # 獲取當前Tqqq價格
-        current_price = self.data["Low"][-1]
-         # 定義一個用於存儲最高價的 Series
-        self.high_prices = self.data["High"]
-        # 獲取過去 80 天的最高價
-        highest_high = self.high_prices[-80:].max()
-
-
-
-
-        if (
-            (
-            (self.data.index[-1].month==9)
-            or
-            ((self.data.index[-1].month==8)and(self.data.index[-1].day==31))
-            or
-            (qqq_current_price <= 0.88 * qqq_highest_high)
-            )
-            and 
-            (self.position.is_long)
-        ):
-            self.position.close()
-            # print(self.data.index[-1],"close")
-            # print('buy_price',buy_price)
-        # 如果 60ma的近20個值不再下跌，買
-        if (
-            not
-            (
-            (self.data.index[-1].month==9)
-            or
-            ((self.data.index[-1].month==8)and(self.data.index[-1].day==31))
-            or
-            (qqq_current_price <= 0.88 * qqq_highest_high)
-            )
-
+        def init(self):
+            self.qqq_sma1 = self.I(SMA, self.data.qqq, self.n1)
+            self.sma1 = self.I(SMA, self.data.Close, self.n1)
+            # self.Vsma1 = self.I(SMA, self.data.Volume, self.n1)
+            # self.ema1 = self.I(EMA30, self.data)
+            self.qqq = self.I(QQQ, window_df["qqq"])
+            self.cooldown_weeks = 6
+            self.last_trade_date = pd.to_datetime("1981-07-31")  # 設定初值
+            # 周範圍
+            self.w_qqq_sma = resample_apply(
+                        'W', SMA, self.data.qqq, 6, plot=True)
+        def next(self): # buy ,close 要互斥
+            # 當前qqq價格
+            qqq_current_price = self.data["qqq"][-1]
+            # 定義一個用於存儲最高價的 Series
+            self.qqq_high_prices = self.data["qqq"]
+            # 獲取過去 80 天的最高價
+            qqq_highest_high = self.qqq_high_prices[-80:].max()
             
-            and
-            (self.w_qqq_sma[-1] - self.w_qqq_sma[-2] > 0)
-            and
-            (not self.position)
+            # 獲取當前Tqqq價格
+            current_price = self.data["Low"][-1]
+            # 定義一個用於存儲最高價的 Series
+            self.high_prices = self.data["High"]
+            # 獲取過去 80 天的最高價
+            highest_high = self.high_prices[-80:].max()
+
+
+
+
+            if (
+                (
+                (self.data.index[-1].month==9)
+                or
+                ((self.data.index[-1].month==8)and(self.data.index[-1].day==31))
+                or
+                (qqq_current_price <= 0.88 * qqq_highest_high)
+                )
+                and 
+                (self.position.is_long)
+            ):
+                self.position.close()
+
+            # 如果 60ma的近20個值不再下跌，買
+            if (
+                not
+                (
+                (self.data.index[-1].month==9)
+                or
+                ((self.data.index[-1].month==8)and(self.data.index[-1].day==31))
+                or
+                (qqq_current_price <= 0.88 * qqq_highest_high)
+                )
+
+                
+                and
+                (self.w_qqq_sma[-1] - self.w_qqq_sma[-2] > 0)
+                and
+                (not self.position)
+                
+            ):
+                self.buy()
+                # print(self.data.index[-1],"buy",self.data.Close[-1])
+                
+    # start_date 和 end_date 加一年
+    start_date = (pd.to_datetime(start_date) + pd.DateOffset(years=1)).strftime("%Y-%m-%d")
+    end_date = (pd.to_datetime(end_date) + pd.DateOffset(years=1)).strftime("%Y-%m-%d")
+    print(window_df.index[0],"-",window_df.index[-1])
             
-        ):
-            self.buy()
-            print(self.data.index[-1],"buy",self.data.Close[-1])
-            
 
 
 
-bt = Backtest(df, MAStra, cash=10000, commission=0.0002,exclusive_orders=True)  # 交易成本 0.0%
-stats = bt.run()
+    bt = Backtest(window_df, TQQQStra, cash=10000, commission=0.0002,exclusive_orders=True)  # 交易成本 0.0%
+    stats = bt.run()
 
-# print(stats)
-print("Buy & Hold Return [%]   ", round(stats["Buy & Hold Return [%]"], 2))
-print("Return (Ann.) [%]       ", round(stats["Return (Ann.) [%]"], 2))
-print("Avg. Drawdown [%]       ", round(stats["Avg. Drawdown [%]"], 2))
-print("Sortino Ratio           ", round(stats["Sortino Ratio"], 2))
-print("Win Rate [%]            ", round(stats["Win Rate [%]"], 2))
+    # print(stats)
+    print("Buy & Hold Return [%]   ", round(stats["Buy & Hold Return [%]"], 2))
+    print("Return [%]              ", round(stats["Return [%]"], 2))
+    print("Return (Ann.) [%]       ", round(stats["Return (Ann.) [%]"], 2))
+    print("Avg. Drawdown [%]       ", round(stats["Avg. Drawdown [%]"], 2))
+    print("Max. Drawdown [%]       ", round(stats["Max. Drawdown [%]"], 2))
+    # print("Avg. Drawdown [%]       ", round(stats["Avg. Drawdown [%]"], 2))
+    print("Sortino Ratio           ", round(stats["Sortino Ratio"], 2))
+    print("Win Rate [%]            ", round(stats["Win Rate [%]"], 2))
+    # 策略贏B&H次數計算
+    if(round(stats["Buy & Hold Return [%]"], 2)-round(stats["Return [%]"], 2))>0:
+        print("Lose ",round(stats["Buy & Hold Return [%]"], 2)-round(stats["Return [%]"], 2), "%")
+        i+=1
+    k+=1
+    
+    #畫return跟B_H的圖
+    str_result.append(round(stats["Return [%]"], 2))
+    b_h_result.append( round(stats["Buy & Hold Return [%]"], 2))
+    
+    print("\n")
 
 
-bt.plot(resample='1W')
+    # bt.plot(resample='1W')
+print(i)
+print("total ",k)
+
+def plot(years,str_result,b_h_result):
+    # 創建DataFrame
+    df = pd.DataFrame({"Year": years, "str_result": str_result,"B&H_result":b_h_result})
+
+    # 繪製折線
+    fig = px.line(df, x="Year", y=["str_result", "B&H_result"], 
+              labels={"value": "return [%]", "variable": "strategy"},
+              title="滾動七年最終報酬")
+
+    fig.show()
+    
+plot(years,str_result,b_h_result)
